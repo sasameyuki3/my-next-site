1ad7b6788a36a58f9e6cfaeb4b7de83e6962e7b2
sasameyuki3 <sasameyuki.reiji@gmail.com>
2025-12-07 15:12:00 +0900

    修正202512071511

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // <--- この1行を追加してください
+  
+  // もし <Image> タグを使う予定があるなら、以下も追加しておくとエラーを防げます
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+
